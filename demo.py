34e9c8ac802599ccfd786c1df5f20dbc477363fe
Vaishnaviiieeee <vaishnavisachinsonawane@gmail.com>
2026-09-10 13:58:37 +0530

    version 1.0.0

---
+++ demo.py
@@ -0,0 +1,11 @@
+
+#vaishnaviieee
+Role= str (input("Enter your role:"))
+Age=int (input("Enter your age:"))
+Eligible = Age >= 21 and Role == "student"
+print("Eligible:", True)
+
+
+#write a program to swap two variables withou a third variable, using arithmetic operations
+#Example: before swap a = 10, b= 20
+#after swap a =20, b=10
